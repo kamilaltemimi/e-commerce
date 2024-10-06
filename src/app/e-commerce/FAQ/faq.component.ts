b04ddc553c0d0e855d5edfd681b57d953892344a
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 22:30:47 +0200

    prepared faq component

---
+++ src/app/e-commerce/FAQ/faq.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-faq',
+    standalone: true,
+    imports: [],
+    templateUrl: './faq.component.html',
+    styleUrl: './faq.component.scss'
+})
+
+export class FaqComponent {
+    
+}
