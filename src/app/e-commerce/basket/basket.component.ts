c5d2d48c0437822795fbf25c12d866a06adf2b30
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 22:31:22 +0200

    prepared basket component

---
+++ src/app/e-commerce/basket/basket.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-basket',
+    standalone: true,
+    imports: [],
+    templateUrl: './basket.component.html',
+    styleUrl: './basket.component.scss'
+})
+
+export class BasketComponent {
+    
+}
