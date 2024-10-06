23d405449057254c1f7f5fdacf6eed265beb374f
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 23:52:46 +0200

    added modal and modal control (component not finished yet)

--- src/app/e-commerce/auth/auth.component.ts
+++ src/app/e-commerce/auth/auth.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { PrivacyNoticeComponent } from '../privacy-notice/privacy-notice.component'
 
 @Component({
     selector: 'app-auth',
     standalone: true,
-    imports: [],
+    imports: [CommonModule, PrivacyNoticeComponent],
     templateUrl: './auth.component.html',
     styleUrl: './auth.component.scss'
 })
 
 export class AuthComponent {
+    isPrivacyNoticeClicked = false
+
+    switchPrivacyNoticeMode(): void {
+        this.isPrivacyNoticeClicked = !this.isPrivacyNoticeClicked
+        console.log(this.isPrivacyNoticeClicked)
+    }
 
 }
