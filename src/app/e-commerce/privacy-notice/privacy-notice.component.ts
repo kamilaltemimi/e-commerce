856283a06af2d5325817f5a34c026b7f8541cf3b
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 23:52:12 +0200

    prepared privacy notice modal component (in progress)

---
+++ src/app/e-commerce/privacy-notice/privacy-notice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-privacy-notice',
+    standalone: true,
+    imports: [],
+    templateUrl: './privacy-notice.component.html',
+    styleUrl: './privacy-notice.component.scss'
+})
+
+export class PrivacyNoticeComponent {
+
+}
