077206417ff36caf8a67147036fa91be97d02081
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 22:30:36 +0200

    prepared main-page component

---
+++ src/app/e-commerce/main-page/main-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-main-page',
+    standalone: true,
+    imports: [],
+    templateUrl: './main-page.component.html',
+    styleUrl: './main-page.component.scss'
+})
+
+export class MainPageComponent {
+    
+}
