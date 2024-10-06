6745b12feb8c74022f56f7cf37025b5ecae2e958
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 22:30:21 +0200

    prepared product component

---
+++ src/app/e-commerce/product/product.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core'
+
+@Component({
+    selector: 'app-product',
+    standalone: true,
+    imports: [],
+    templateUrl: './product.component.html',
+    styleUrl: './product.component.scss'
+})
+
+export class ProductComponent {
+    
+}
