5083c6328b51a4298dfad3d974c4c210c9e98f49
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-06 22:34:49 +0200

    added main page route and auth route. Main page is using AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MainPageComponent } from './e-commerce/main-page/main-page.component';
+import { AuthGuard } from './core/guards/auth.guard';
+import { AuthComponent } from './e-commerce/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', component: MainPageComponent, canActivate: [AuthGuard]},
+    { path: 'auth', component: AuthComponent}
+
+];
