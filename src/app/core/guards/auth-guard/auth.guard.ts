f1af5be22f420a33cac48a64c193ea524d60d84e
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-07 22:13:03 +0200

    added auth guard

---
+++ src/app/core/guards/auth-guard/auth.guard.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core'
+import { CanActivate, Router } from '@angular/router'
+
+import { AuthService } from '../../services/auth/auth.service'
+
+import { User } from '../../interfaces/user.interface'
+
+import { map, Observable } from 'rxjs'
+
+@Injectable({providedIn: 'root'})
+
+export class AuthGuard implements CanActivate {
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ) {}
+
+    canActivate(): Observable<boolean> {
+        return this.authService.isLoggedInObs.pipe(
+            map((data: User | null) => {
+                if (data) {
+                    return true
+                } else {
+                    this.router.navigate(['auth'])
+                    return false
+                }
+            })
+        )
+    }
+}
