1ff618dde31aff29d6945ee1a225e11118edd8fd
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-07 22:14:22 +0200

    created user service with get users method (from firebase)

---
+++ src/app/core/services/user/user.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+
+import { map, Observable } from 'rxjs'
+
+import { User } from '../../interfaces/user.interface'
+
+@Injectable({providedIn: 'root'})
+
+export class UserService {
+
+    URL = 'https://e-commerce-b1516-default-rtdb.firebaseio.com'
+
+    constructor(
+        private http: HttpClient
+    ) {}
+    
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(`${this.URL}/users.json`).pipe(
+            map((users: User[]) => {
+                let userArray: User[] = []
+                for (const key in users) {
+                    userArray.push({...users[key]})
+                } return userArray
+            })
+        )
+    }
+}
