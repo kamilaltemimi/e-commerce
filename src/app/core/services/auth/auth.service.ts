bd8e658b2da96833a1778716672ab13bd8a3f7f1
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-07 15:51:19 +0200

    added 2 methods - getting users from database and registering user to the database

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core'
 
-import { BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
 
 import { User } from '../../interfaces/user.interface'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({providedIn: 'root'})
 
@@ -11,4 +12,17 @@ export class AuthService {
     private isLoggedIn = new BehaviorSubject<User | null>(null)
     public isLoggedInObs = this.isLoggedIn.asObservable()
 
+    URL = 'https://e-commerce-b1516-default-rtdb.firebaseio.com'
+
+    constructor(
+        private http: HttpClient
+    ) {}
+
+    getUsers(): Observable<User[]> {
+        return this.http.get<User[]>(`${this.URL}/users.json`)
+    }
+
+    registerUser(user: User): Observable<User> {
+        return this.http.post<User>(`${this.URL}/users.json`, user)
+    }
 }
