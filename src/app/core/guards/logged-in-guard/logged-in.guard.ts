a895a949cce599d609571deeff725048238c7a3c
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-07 22:13:26 +0200

    added another guard (if you are logged in you cant visit auth path)

---
+++ src/app/core/guards/logged-in-guard/logged-in.guard.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core'
+import { CanActivate } from '@angular/router'
+
+import { map, Observable } from 'rxjs'
+import { AuthService } from '../../services/auth/auth.service'
+import { User } from '../../interfaces/user.interface'
+
+@Injectable({providedIn: 'root'})
+
+export class LoggedInGuard implements CanActivate {
+
+    constructor(
+        private authService:  AuthService
+    ) {}
+    
+    canActivate(): Observable<boolean> {
+        return this.authService.isLoggedInObs.pipe(
+            map((data: User | null) => {
+                if (!data) {
+                    return true
+                } else {
+                    return false
+                }
+            })
+        )
+    }
+}
