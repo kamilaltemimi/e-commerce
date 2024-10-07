5177cb0fce73609bf54caf26d3bdcc7e16d2f04d
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-07 22:12:48 +0200

    added guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,13 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './e-commerce/main-page/main-page.component';
-import { AuthGuard } from './core/guards/auth.guard';
+import { AuthGuard } from './core/guards/auth-guard/auth.guard';
 import { AuthComponent } from './e-commerce/auth/auth.component';
+import { LoggedInGuard } from './core/guards/logged-in-guard/logged-in.guard';
 
 export const routes: Routes = [
 
     { path: '', component: MainPageComponent, canActivate: [AuthGuard]},
-    { path: 'auth', component: AuthComponent}
+    { path: 'auth', component: AuthComponent, canActivate: [LoggedInGuard]},
+    { path: '**', redirectTo: ''}
 
 ];
