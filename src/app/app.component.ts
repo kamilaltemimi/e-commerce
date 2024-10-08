3f7ea7021101b727ad9ef495cade986b9015484e
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-08 23:25:24 +0200

    added header to app component and provided visibility control whether user is logged in or not

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+
 import { AuthService } from './core/services/auth/auth.service';
+
+import { HeaderComponent } from './shared/header/header.component';
+
+import { User } from './core/interfaces/user.interface';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
+
+  isLoggedIn: User | null = null
 
   constructor(
     private authService: AuthService
@@ -21,6 +29,9 @@ export class AppComponent implements OnInit {
       const parsedData = JSON.parse(userData)
       this.authService.changeIsLoggedInStatus(parsedData)
     }
+    this.authService.isLoggedInObs.subscribe((status: User | null) => {
+      this.isLoggedIn = status
+    })
   }
 
 }
