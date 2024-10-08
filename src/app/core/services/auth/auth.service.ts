d1550c246b22a80c70f14e1588eb336eb8c9ed96
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-08 23:26:19 +0200

    added logout method to return to the auth page after logout

--- src/app/core/services/auth/auth.service.ts
+++ src/app/core/services/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, Observable } from 'rxjs'
 
 import { User } from '../../interfaces/user.interface'
 import { HttpClient } from '@angular/common/http'
+import { Router } from '@angular/router'
 
 @Injectable({providedIn: 'root'})
 
@@ -15,11 +16,18 @@ export class AuthService {
     URL = 'https://e-commerce-b1516-default-rtdb.firebaseio.com'
 
     constructor(
-        private http: HttpClient
+        private http: HttpClient,
+        private router: Router
     ) {}
 
     changeIsLoggedInStatus(user: User): void {
         this.isLoggedIn.next(user)
+    }
+
+    logout(): void {
+        this.isLoggedIn.next(null)
+        localStorage.removeItem('user')
+        this.router.navigate(['/auth'])
     }
 
     registerUser(user: User): Observable<User> {
