b9ef4754b660a88c178990f40fe402de0b4b5f11
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 14:48:41 +0200

    added product category enum

---
+++ src/app/core/enums/product-category/product-category.enum.ts
@@ -0,0 +1,10 @@
+export enum ProductCategory {
+    bags = 'bags',
+    gloves = 'gloves',
+    head = 'headguards',
+    heavybags = 'heavybags',
+    mouthguards = 'mouthguards',
+    pads = 'pads',
+    shoes = 'shoes',
+    wraps = 'wraps'
+}
