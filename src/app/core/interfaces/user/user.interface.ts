504a12d04cf1d0a74e034143ee71517b7a29c5a7
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 23:32:17 +0200

    configured user interface

--- src/app/core/interfaces/user/user.interface.ts
+++ src/app/core/interfaces/user/user.interface.ts
@@ -2,7 +2,5 @@ import { Product } from "../product/product.interface"
 
 export interface User {
     email: string,
-    password: string,
-    selectedProduct?: Product,
-    basket?: Product[]
+    password: string
 }
