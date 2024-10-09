b1acbdd815220b11ed834a37b1bf2c4aeebdc958
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 14:52:45 +0200

    added and configured product component. protected data from refreshing. fetched product from service and added unsubscription on destroy

--- src/app/e-commerce/product/product.component.ts
+++ src/app/e-commerce/product/product.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core'
+import { Component, OnDestroy, OnInit } from '@angular/core'
+
+import { ProductsService } from '../../core/services/products/products.service';
+
+import { Product } from '../../core/interfaces/product/product.interface';
+
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
     selector: 'app-product',
@@ -8,6 +14,41 @@ import { Component } from '@angular/core'
     styleUrl: './product.component.scss'
 })
 
-export class ProductComponent {
-    
+export class ProductComponent implements OnInit, OnDestroy {
+
+    selectedProduct: Product | null = null
+
+    private unsubscribe$ = new Subject<void>()
+
+    constructor(
+        private productsService: ProductsService
+    ) {}
+
+    ngOnInit(): void {
+        this.getSelectedProductsInCaseOfRefreshingPage()
+        this.getSelectedProductData()
+    }
+
+    ngOnDestroy(): void {
+        this.unsubscribe$.next()
+        this.unsubscribe$.complete()
+    }
+
+    getSelectedProductData(): void {
+        this.productsService.selectedProductObs
+        .pipe(takeUntil(this.unsubscribe$))
+        .subscribe((data: Product | null) => {
+            this.selectedProduct = data
+        })
+    }
+
+    getSelectedProductsInCaseOfRefreshingPage(): void {
+        const storedProduct = localStorage.getItem('selectedProduct')
+        
+        if (storedProduct) {
+            const product = JSON.parse(storedProduct)
+            this.productsService.selectProduct(product)
+        }
+    }
+
 }
