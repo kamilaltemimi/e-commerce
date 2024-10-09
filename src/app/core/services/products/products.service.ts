30c65f7dc32dbc04edd04436ee7c3aceab0edd18
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 14:51:36 +0200

    added and configured products service but still working on it. added path to products. created behavior subject and made 2 methods: getallproducts and selectproduct

---
+++ src/app/core/services/products/products.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core'
+
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { Product } from '../../interfaces/product/product.interface';
+
+import { RoutingService } from '../routing/routing.service';
+
+@Injectable({providedIn: 'root'})
+
+export class ProductsService {
+
+    private selectedProduct = new BehaviorSubject<Product | null>(null)
+    selectedProductObs = this.selectedProduct.asObservable()
+
+    URL = 'assets/data/products.json'
+
+    constructor(
+        private http: HttpClient,
+        private routingService: RoutingService
+    ) {}
+
+    getAllProducts(): Observable<Product[]> {
+        return this.http.get<Product[]>(this.URL)
+    }
+
+    selectProduct(product: Product): void {
+        this.selectedProduct.next(product)
+        this.routingService.navigate(`product/${product.name}`)
+        localStorage.setItem('selectedProduct', JSON.stringify(product))
+    }
+}
