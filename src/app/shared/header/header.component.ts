7e25a911f5dd99885c6aeeecb65f8acc98562791
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 23:07:45 +0200

    configured header to show basket quantity

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,22 +1,38 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 
 import { AuthService } from '../../core/services/auth/auth.service';
 import { RoutingService } from '../../core/services/routing/routing.service';
+import { BasketService } from '../../core/services/basket/basket.service';
+
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-header',
     standalone: true,
-    imports: [],
+    imports: [CommonModule],
     templateUrl: './header.component.html',
     styleUrl: './header.component.scss'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+
+    public basketQuantity: number  = 0
 
     constructor(
         private authService: AuthService,
-        private routingService: RoutingService
+        private routingService: RoutingService,
+        private basketService: BasketService
     ) {}
+
+    ngOnInit(): void {
+        this.getBasketQuantity()
+    }
+
+    getBasketQuantity(): void {
+        this.basketService.basketQuantity$.subscribe((value: number) => {
+            this.basketQuantity = value
+        })
+    }
 
     navigate(path: string): void {
         this.routingService.navigate(path)
