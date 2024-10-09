13fff8b3ce1858524c116926c56da03df749b707
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-09 23:08:38 +0200

    configured basket to be updated and setted up local storage to protect basket from refreshing page

--- src/app/core/services/basket/basket.service.ts
+++ src/app/core/services/basket/basket.service.ts
@@ -1,24 +1,45 @@
 import { Injectable } from '@angular/core'
 
 import { Product } from '../../interfaces/product/product.interface'
+import { BehaviorSubject } from 'rxjs'
 
 @Injectable({providedIn: 'root'})
 
-
 export class BasketService {
+
+    private basket: Product[] = []
+
+    private basketQuantity = new BehaviorSubject<number>(0)
+    public basketQuantity$ = this.basketQuantity.asObservable()
+
+    private basketBehaviorSubject = new BehaviorSubject<Product[]>(this.basket)
+    public basketData$ = this.basketBehaviorSubject.asObservable()
 
     constructor() {}
 
-    basket: Product[] = []
+    set setBasketData(products: Product[]) {
+        this.basket = products
+    }
+
+    set setBasketQuantity(value: number) {
+        this.basketQuantity.next(value)
+    }
 
     addItem(item: Product): void {
-        let existingProductIndex = this.basket.findIndex((product: Product) => product.id === item.id)
+        const existingProductIndex = this.basket.findIndex((product: Product) => product.id === item.id)
 
-        if (existingProductIndex) {
-            this.basket[existingProductIndex].quantity!++
+        this.basketQuantity.next(this.basketQuantity.value + 1)
+
+        if (existingProductIndex !== -1) {
+            this.basket[existingProductIndex].quantity++
         } else {
-            this.basket.push({...item, quantity: 1})
+            this.basket.push(item)
         }
-        console.log(this.basket)
+        this.updateLocalStorage()
+    }
+
+    updateLocalStorage(): void {
+        localStorage.setItem('basket', JSON.stringify(this.basket))
+        localStorage.setItem('basketQuantity', JSON.stringify(this.basketQuantity.value))
     }
 }
