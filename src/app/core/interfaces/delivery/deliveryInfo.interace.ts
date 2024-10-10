d50328e5cc077c364eab0c9d33b02063e080d47a
kamilaltemimi <kamilaltemimi@gmail.com>
2024-10-10 23:30:18 +0200

    created interface fo delivery info

---
+++ src/app/core/interfaces/delivery/deliveryInfo.interace.ts
@@ -0,0 +1,10 @@
+export interface DeliveryInfo {
+    email: string,
+    fullName: string,
+    address: string,
+    zip: string,
+    nameOnCard: string,
+    creditCardNumber: string,
+    expiration: string,
+    cvv: string
+}
